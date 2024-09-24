f99afd9532b01e0b0690a101e108108304eb9399
MuneebKhan46 <81796045+MuneebKhan46@users.noreply.github.com>
2024-09-24 14:38:11 +0900

    Update ARCNN2.py

--- Code/N0/ARCNN2.py
+++ Code/N0/ARCNN2.py
@@ -102,7 +102,6 @@ def load_data_from_csv(csv_path, original_dir, denoised_dir):
     all_patch_numbers = []
 
     for _, row in df.iterrows():
-        # Load only .raw YUV files
         original_file_name = f"original_{row['image_name']}.raw"
         denoised_file_name = f"denoised_{row['image_name']}.raw"
 
@@ -164,7 +163,8 @@ n0_count, non_n0_count = dataset.get_image_counts()
 
 print(f"Total images with 'n0': {n0_count}")
 print(f"Total images without 'n0': {non_n0_count}")
-
+orig, den = dataset[0]
+print(orig.shape, den.shape)
 
 train_data, temp_data = train_test_split(dataset, test_size=0.2, random_state=42)
 val_data, test_data = train_test_split(temp_data, test_size=0.5, random_state=42)
@@ -181,151 +181,151 @@ val_loader = DataLoader(val_data,  shuffle=False)
 test_loader = DataLoader(test_data, shuffle=False)
 
 
-model = ARCNN()
-model = nn.DataParallel(model)
-model = model.to(device)
+# model = ARCNN()
+# model = nn.DataParallel(model)
+# model = model.to(device)
 
-criterion = nn.MSELoss()
-optimizer = torch.optim.Adam(model.parameters(), lr=5e-4)
-best_val_loss = float('inf')
+# criterion = nn.MSELoss()
+# optimizer = torch.optim.Adam(model.parameters(), lr=5e-4)
+# best_val_loss = float('inf')
 
 
-for epoch in range(EPOCHS):
+# for epoch in range(EPOCHS):
     
-    model.train()
-    total_loss = 0
+#     model.train()
+#     total_loss = 0
 
-    for original, denoised in train_loader:
-        original, denoised = original.to(device), denoised.to(device)
+#     for original, denoised in train_loader:
+#         original, denoised = original.to(device), denoised.to(device)
         
-        output = model(denoised)
+#         output = model(denoised)
         
-        loss = criterion(output, original)
+#         loss = criterion(output, original)
         
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step() 
+#         optimizer.zero_grad()
+#         loss.backward()
+#         optimizer.step() 
 
-        total_loss += loss.item()
+#         total_loss += loss.item()
 
-    print(f"Epoch {epoch+1}/{EPOCHS}, Training Loss: {total_loss / len(train_loader):.4f}")
+#     print(f"Epoch {epoch+1}/{EPOCHS}, Training Loss: {total_loss / len(train_loader):.4f}")
   
-    model.eval()
-    val_loss = 0.0
+#     model.eval()
+#     val_loss = 0.0
     
-    with torch.no_grad():
-        for original_val, denoised_val in val_loader:
-            original_val, denoised_val = original_val.to(device), denoised_val.to(device)
+#     with torch.no_grad():
+#         for original_val, denoised_val in val_loader:
+#             original_val, denoised_val = original_val.to(device), denoised_val.to(device)
             
-            outputs_val = model(denoised_val)
+#             outputs_val = model(denoised_val)
             
-            loss = criterion(outputs_val, original_val)
+#             loss = criterion(outputs_val, original_val)
             
-            val_loss += loss.item()
+#             val_loss += loss.item()
 
-    val_loss /= len(val_loader)
+#     val_loss /= len(val_loader)
     
-    print(f"Epoch {epoch+1}/{EPOCHS}, Validation Loss: {val_loss:.4f}")
+#     print(f"Epoch {epoch+1}/{EPOCHS}, Validation Loss: {val_loss:.4f}")
   
-    if val_loss < best_val_loss:
-        best_val_loss = val_loss
-        torch.save(model.state_dict(), os.path.join(model_dir, 'HighFreq_ARCNN_Model(N0).pth'))
-        print(f"New best model saved with validation loss: {val_loss:.4f}")
+#     if val_loss < best_val_loss:
+#         best_val_loss = val_loss
+#         torch.save(model.state_dict(), os.path.join(model_dir, 'HighFreq_ARCNN_Model(N0).pth'))
+#         print(f"New best model saved with validation loss: {val_loss:.4f}")
 
 
 
 
-model.load_state_dict(torch.load(os.path.join(model_dir, 'HighFreq_ARCNN_Model(N0).pth')))
-model.eval()
+# model.load_state_dict(torch.load(os.path.join(model_dir, 'HighFreq_ARCNN_Model(N0).pth')))
+# model.eval()
 
-def save_image(image_tensor, filename):
-    image_array = (image_tensor * 255).clamp(0, 255).byte().permute(1, 2, 0).cpu().numpy()
-    image_pil = Image.fromarray(image_array)
-    image_pil.save(filename)
+# def save_image(image_tensor, filename):
+#     image_array = (image_tensor * 255).clamp(0, 255).byte().permute(1, 2, 0).cpu().numpy()
+#     image_pil = Image.fromarray(image_array)
+#     image_pil.save(filename)
 
 
-def visualize_and_save_patches(original, denoised, restored, idx):
-    if isinstance(original, np.ndarray):
-        original = torch.tensor(original)
-    if isinstance(denoised, np.ndarray):
-        denoised = torch.tensor(denoised)
-    if isinstance(restored, np.ndarray):
-        restored = torch.tensor(restored)
+# def visualize_and_save_patches(original, denoised, restored, idx):
+#     if isinstance(original, np.ndarray):
+#         original = torch.tensor(original)
+#     if isinstance(denoised, np.ndarray):
+#         denoised = torch.tensor(denoised)
+#     if isinstance(restored, np.ndarray):
+#         restored = torch.tensor(restored)
     
-    original_file = os.path.join(image_save_dir, f"ARCNN_original_patch_{idx}.png")
-    denoised_file = os.path.join(image_save_dir, f"ARCNN_denoised_patch_{idx}.png")
-    restored_file = os.path.join(image_save_dir, f"ARCNN_restored_patch_{idx}.png")
+#     original_file = os.path.join(image_save_dir, f"ARCNN_original_patch_{idx}.png")
+#     denoised_file = os.path.join(image_save_dir, f"ARCNN_denoised_patch_{idx}.png")
+#     restored_file = os.path.join(image_save_dir, f"ARCNN_restored_patch_{idx}.png")
     
-    save_image(original, original_file)
-    save_image(denoised, denoised_file)
-    save_image(restored, restored_file)
+#     save_image(original, original_file)
+#     save_image(denoised, denoised_file)
+#     save_image(restored, restored_file)
 
-    # fig, axs = plt.subplots(1, 3, figsize=(15, 5))
-    # axs[0].imshow(original.permute(1, 2, 0).cpu().numpy())
-    # axs[0].set_title("Original Image")
-    # axs[1].imshow(denoised.permute(1, 2, 0).cpu().numpy())
-    # axs[1].set_title("Denoised Image")
-    # axs[2].imshow(restored.permute(1, 2, 0).cpu().numpy())
-    # axs[2].set_title("ARCNN")
-    # for ax in axs:
-    #     ax.axis('off')
-    # plt.show()
+#     # fig, axs = plt.subplots(1, 3, figsize=(15, 5))
+#     # axs[0].imshow(original.permute(1, 2, 0).cpu().numpy())
+#     # axs[0].set_title("Original Image")
+#     # axs[1].imshow(denoised.permute(1, 2, 0).cpu().numpy())
+#     # axs[1].set_title("Denoised Image")
+#     # axs[2].imshow(restored.permute(1, 2, 0).cpu().numpy())
+#     # axs[2].set_title("ARCNN")
+#     # for ax in axs:
+#     #     ax.axis('off')
+#     # plt.show()
 
-psnr_scores, ssim_scores = [], []
-results = []
+# psnr_scores, ssim_scores = [], []
+# results = []
 
-image_save_dir = os.path.join(Results_dir, 'Images/AR-CNN(N0)/')
-os.makedirs(image_save_dir, exist_ok=True)
+# image_save_dir = os.path.join(Results_dir, 'Images/AR-CNN(N0)/')
+# os.makedirs(image_save_dir, exist_ok=True)
 
-visualized_images = 0
-visualize_limit = 10
+# visualized_images = 0
+# visualize_limit = 10
 
 
-with torch.no_grad():
-    for original_test, denoised_test in test_loader:
-        original_test, denoised_test = original_test.to(device), denoised_test.to(device)
+# with torch.no_grad():
+#     for original_test, denoised_test in test_loader:
+#         original_test, denoised_test = original_test.to(device), denoised_test.to(device)
         
-        outputs_test = model(denoised_test)
+#         outputs_test = model(denoised_test)
         
-        outputs_test = outputs_test.cpu().numpy()
-        original_test = original_test.cpu().numpy()
+#         outputs_test = outputs_test.cpu().numpy()
+#         original_test = original_test.cpu().numpy()
 
-        for i in range(len(outputs_test)):
+#         for i in range(len(outputs_test)):
             
-            psnr_scores.append(psnr(original_test[i], outputs_test[i]))
+#             psnr_scores.append(psnr(original_test[i], outputs_test[i]))
             
-            patch_size = min(outputs_test[i].shape[0], outputs_test[i].shape[1])
-            win_size = min(7, patch_size) 
+#             patch_size = min(outputs_test[i].shape[0], outputs_test[i].shape[1])
+#             win_size = min(7, patch_size) 
             
-            if win_size >= 3:
-                ssim_val = ssim(original_test[i], outputs_test[i], win_size=win_size, channel_axis=-1, data_range=1.0)
-                ssim_scores.append(ssim_val)
-            else:
-                print(f"Skipping SSIM for patch {i} due to insufficient size")
+#             if win_size >= 3:
+#                 ssim_val = ssim(original_test[i], outputs_test[i], win_size=win_size, channel_axis=-1, data_range=1.0)
+#                 ssim_scores.append(ssim_val)
+#             else:
+#                 print(f"Skipping SSIM for patch {i} due to insufficient size")
 
-            if visualized_images < visualize_limit:
-              visualize_and_save_patches(original_test[i], denoised_test[i], outputs_test[i], visualized_images)
-              visualized_images += 1
+#             if visualized_images < visualize_limit:
+#               visualize_and_save_patches(original_test[i], denoised_test[i], outputs_test[i], visualized_images)
+#               visualized_images += 1
 
    
 
 
-avg_psnr = np.mean(psnr_scores)
-avg_ssim = np.mean(ssim_scores) if ssim_scores else 0
+# avg_psnr = np.mean(psnr_scores)
+# avg_ssim = np.mean(ssim_scores) if ssim_scores else 0
 
-print(f"Average PSNR: {avg_psnr:.4f}")
-print(f"Average SSIM: {avg_ssim:.4f}") 
+# print(f"Average PSNR: {avg_psnr:.4f}")
+# print(f"Average SSIM: {avg_ssim:.4f}") 
 
-results_csv_path = os.path.join(Results_dir, 'results.csv')
+# results_csv_path = os.path.join(Results_dir, 'results.csv')
 
-results.append({'Model': 'AR-CNN',  'Image Type': 'RGB', 'Noise Type': 'N0', 'PSNR': avg_psnr, 'SSIM': avg_ssim})
+# results.append({'Model': 'AR-CNN',  'Image Type': 'RGB', 'Noise Type': 'N0', 'PSNR': avg_psnr, 'SSIM': avg_ssim})
 
-if os.path.exists(results_csv_path):
-    df_existing = pd.read_csv(results_csv_path)
-    df_new = pd.DataFrame(results)
-    df_results = pd.concat([df_existing, df_new], ignore_index=True)
-else:
-    df_results = pd.DataFrame(results)
-df_results.to_csv(results_csv_path, index=False)
+# if os.path.exists(results_csv_path):
+#     df_existing = pd.read_csv(results_csv_path)
+#     df_new = pd.DataFrame(results)
+#     df_results = pd.concat([df_existing, df_new], ignore_index=True)
+# else:
+#     df_results = pd.DataFrame(results)
+# df_results.to_csv(results_csv_path, index=False)
 
-print(f"Results saved to {results_csv_path}")
+# print(f"Results saved to {results_csv_path}")
